f2f4a88959bbb9ab18f2259104ba123428fd8338
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-08-17 18:07:17 -0700

    Create 57. Insert Interval

---
+++ 57. Insert Interval.js
@@ -0,0 +1,22 @@
+var insert = function(intervals, newInterval) {
+  intervals = [...intervals, newInterval];
+
+  intervals.sort((a, b) => a[0] - b[0]);
+  let curr = intervals[0], prev = intervals[0];
+  const [start, end] = [0, 1];
+  const result = [];
+  for (let i = 0; i < intervals.length; i++) {
+    curr = intervals[i];
+
+    if (curr[start] <= prev[end] && curr[end] > prev[end]) {
+      prev[end] = curr[end];
+    } else if (curr[start] > prev[end]) {
+      result.push(prev);
+      prev = curr;
+    }
+
+    if (i == intervals.length - 1) result.push(prev);
+  }
+  return result;
+};
+
