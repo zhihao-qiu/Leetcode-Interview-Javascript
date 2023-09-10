32cdbdcbf844445f151ea7a3dd214b9a709103d7
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-09-09 22:19:44 -0700

    Create 101. Symmetric Tree

---
+++ 101. Symmetric Tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+  if (!root) return true;
+
+  const isMirror = (left, right) => {
+    if (!left && !right) return true;
+    if (!left || !right) return false;
+
+    return (left.val == right.val && isMirror(left.left, right.right) && isMirror(left.right, right.left));
+  };
+  return isMirror(root.left, root.right);
+};
